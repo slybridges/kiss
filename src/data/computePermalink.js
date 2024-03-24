06c60a95c43f01db2be1c3a75eb0cd44a50c8f39
Sylvestre Dupont <sylvestre@wildebrug.com>
2024-03-24 16:44:24 +0400

    Improve computePermalink to take into account parent permalink

--- src/data/computePermalink.js
+++ src/data/computePermalink.js
@@ -1,8 +1,10 @@
-const computePermalink = ({ slug, _meta }, config) => {
+const path = require("path")
+
+const computePermalink = ({ slug, _meta }, config, { pages }) => {
   // remove top level dir (dirs.content)
   let permalink = _meta.inputPath.replace(
     new RegExp(`^${config.dirs.content}`),
-    ""
+    "",
   )
   if (_meta.outputType === "HTML") {
     permalink = permalink
@@ -12,14 +14,47 @@ const computePermalink = ({ slug, _meta }, config) => {
       // remove extensions
       .replace(new RegExp(/\.[a-z]+$/), "")
   }
+  let basePermalink
   if (slug) {
-    // if a slug was provided, replace the last meaningful bit of the url with it
-    permalink = permalink.replace(new RegExp(/\/[^/]+\/?$/), `/${slug}`)
+    // if the basename starts with post or index, the slug will be a directory
+    if (
+      _meta.basename.startsWith("post.") ||
+      _meta.basename.startsWith("index.")
+    ) {
+      slug += "/"
+    }
+  } else {
+    const parts = permalink.split("/")
+    // get the slug from the last non empty part of the permalink
+    if (permalink.endsWith("/")) {
+      slug = parts[parts.length - 2] + "/"
+    } else {
+      slug = parts[parts.length - 1]
+    }
   }
+
+  if (_meta.parent) {
+    // we want to look into the parent in case they have a customized permalink
+    const parent = pages[_meta.parent]
+    if (typeof parent.permalink === "function") {
+      // return the function itself hoping parent permalint will be computed later
+      return computePermalink({ slug, _meta }, config, { pages })
+    }
+    if (typeof parent.permalink === "string") {
+      basePermalink = parent.permalink
+    }
+  }
+  if (!basePermalink) {
+    // basePermalink is everything before the slug
+    basePermalink = permalink.slice(0, -slug.length)
+  }
+  permalink = path.join(basePermalink, slug)
   if (_meta.isDirectory && !permalink.endsWith("/")) {
     permalink += "/"
   }
   return permalink
 }
+
+computePermalink.kissDependencies = ["slug", ["_meta.parent", "permalink"]]
 
 module.exports = computePermalink
