ca3cca185ccc7d5aa9178cba1ad12d736f3268dd
Sylvestre Dupont <sylvestre@wildebrug.com>
2024-03-26 15:43:45 +0400

    For collection take the earlier created date and latestModified date of descendants by default

--- src/data/computeCreated.js
+++ src/data/computeCreated.js
@@ -1,7 +1,25 @@
-const computeCreated = ({ _meta }) => {
+const computeCreated = ({ _meta }, config, { pages }) => {
+  // take the earliest created date of all descendants
+  if (_meta.isCollection) {
+    const earliest = _meta.descendants.reduce((earliest, pageId) => {
+      const created = pages[pageId][config.defaults.pagePublishedAttribute]
+      // assume an date is a valid Date object if it has a getMonth function
+      if (!created || typeof created?.getMonth !== "function") {
+        // invalid or net yet computed
+        return earliest
+      }
+      return created < earliest ? created : earliest
+    }, Infinity)
+    if (earliest !== Infinity) {
+      return earliest
+    }
+  }
+
   if (_meta.fileCreated) {
     return _meta.fileCreated
   }
 }
+
+computeCreated.kissDependencies = [["_meta.descendants", "created"]]
 
 module.exports = computeCreated
--- src/data/computeModified.js
+++ src/data/computeModified.js
@@ -1,7 +1,24 @@
-const computeModified = ({ _meta }) => {
+const computeModified = ({ _meta }, config, { pages }) => {
+  // take the latest modified date of all descendants
+  if (_meta.isCollection) {
+    const latest = _meta.descendants.reduce((latest, pageId) => {
+      const modified = pages[pageId][config.defaults.pageUpdatedAttribute]
+      // assume an date is a valid Date object if it has a getMonth function
+      if (!modified || typeof modified?.getMonth !== "function") {
+        // invalid or net yet computed
+        return latest
+      }
+      return modified > latest ? modified : latest
+    }, 0)
+    if (latest !== 0) {
+      return latest
+    }
+  }
   if (_meta.fileModified) {
     return _meta.fileModified
   }
 }
+
+computeModified.kissDependencies = [["_meta.descendants", "modified"]]
 
 module.exports = computeModified
