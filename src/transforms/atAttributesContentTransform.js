11f6509cf400c9d3408deb2665ff7020d64e6781
Sylvestre Dupont <sylvestre@wildebrug.com>
2024-03-27 15:20:37 +0400

    Add @data attribute and improve error messages

--- src/transforms/atAttributesContentTransform.js
+++ src/transforms/atAttributesContentTransform.js
@@ -1,8 +1,9 @@
+const _ = require("lodash")
+
 const { AT_GENERIC_ATTRIBUTE_REGEX, isValidURL } = require("../helpers")
 
 const atAttributesContentTransform = (page, options, config, context) => {
   let errorCount = 0
-  page = { ...page }
   // we need to go though all the keys in the page
   // (that may be expesive, but yolo)
   for (const objKey in page) {
@@ -40,7 +41,7 @@ const transformAtAttributesinObjValue = (
   config,
   context,
 ) => {
-  let pageFound = null
+  let valueFound = null
   let errorCount = 0
   let match
   if (typeof objValue === "object") {
@@ -59,24 +60,16 @@ const transformAtAttributesinObjValue = (
   while ((match = AT_GENERIC_ATTRIBUTE_REGEX.exec(objValue))) {
     let [fullMatch, attribute, value] = match
     switch (attribute) {
-      case "permalink":
-        // Replace the fullMach with the permalink even if there are errors down the line
-        objValue = objValue.replaceAll(fullMatch, value)
-        if (isValidURL(value)) {
-          global.logger.warn(
-            `Key '${objKey}' in page '${page.permalink}': @permalink '${value}' is a URL. Skipping.`,
-          )
-          continue
-        }
-        // check if we have a page with this permalink
-        pageFound = Object.values(context.pages).find(
-          (page) => page.permalink === value,
-        )
-        if (!pageFound) {
-          // we have an error
+      case "data":
+        // Data references a path from the context
+        valueFound = _.get(context, value)
+        if (valueFound) {
+          objValue = objValue.replaceAll(fullMatch, valueFound)
+        } else {
           global.logger.error(
-            `Key '${objKey}' in page '${page.permalink}': no page found with @permalink '${value}'`,
+            `Page '${page.permalink}' in '${objKey}': @data not found '${value}'`,
           )
+          objValue = objValue.replaceAll(fullMatch, value)
           errorCount++
         }
         break
@@ -84,20 +77,57 @@ const transformAtAttributesinObjValue = (
         // At this stage, we know we have the full path already
         // it was computed during file loading
         // Search if a have a page with that inputPath
-        pageFound = findPageByFilepath(value, page, config, context)
-        if (pageFound) {
+        valueFound = findPageByFilepath(value, page, config, context)
+        if (valueFound) {
+          if (valueFound.excludeFromWrite) {
+            global.logger.error(
+              `Page '${page.permalink}' in '${objKey}': @file found '${value}' but page is marked as excludeFromWrite`,
+            )
+            errorCount++
+          }
           // found the corresponding page: replace all occurences with permalink
-          objValue = objValue.replaceAll(fullMatch, pageFound.permalink)
+          objValue = objValue.replaceAll(fullMatch, valueFound.permalink)
         } else {
           // something's wrong
+          global.logger.error(
+            `Page '${page.permalink}' in '${objKey}': @file not found '${value}'`,
+          )
           // remove the @file attribute in case the file is found magically later
           objValue = objValue.replaceAll(fullMatch, value)
           errorCount++
         }
         break
+      case "permalink":
+        // Replace the fullMach with the permalink even if there are errors down the line
+        objValue = objValue.replaceAll(fullMatch, value)
+        if (isValidURL(value)) {
+          global.logger.warn(
+            `Page '${page.permalink}' in '${objKey}': @permalink '${value}' is a URL. Skipping.`,
+          )
+          continue
+        }
+        // check if we have a page with this permalink
+        valueFound = Object.values(context.pages).find(
+          (page) => page.permalink === value,
+        )
+        if (valueFound) {
+          if (valueFound.excludeFromWrite) {
+            global.logger.error(
+              `Page '${page.permalink}' in '${objKey}': @permalink found '${value}' but page is marked as excludeFromWrite`,
+            )
+            errorCount++
+          }
+        } else {
+          // we have an error
+          global.logger.error(
+            `Page '${page.permalink}' in '${objKey}': @permalink not found '${value}'`,
+          )
+          errorCount++
+        }
+        break
       default:
         global.logger.error(
-          `Key '${objKey}' in page '${page.permalink}': unknown @attribute '${attribute}'`,
+          `Page '${page.permalink}' in '${objKey}': unknown @attribute '${attribute}'`,
         )
         errorCount++
     }
@@ -109,10 +139,5 @@ const findPageByFilepath = (filepath, page, config, context) => {
   const pageFound = Object.values(context.pages).find(
     (page) => page._meta.inputPath === filepath,
   )
-  if (pageFound) {
-    return pageFound
-  }
-  global.logger.error(
-    `Page '${page.permalink}': no page found for @file '${filepath}' in ${filepath}`,
-  )
+  return pageFound
 }
